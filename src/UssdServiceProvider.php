14b612bbc42117c0f5e221a2c508b270727f44a1
Cyril Aguvasu <aguvasucyril@gmail.com>
2023-06-05 23:47:23 +0300

    provider class

---
+++ src/UssdServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Stilinski\Ussd;
+
+use Illuminate\Support\ServiceProvider;
+
+class UssdServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'stilinski');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->publishes([
+            __DIR__.'/../config/ussd.php' => config_path('ussd.php'),
+            __DIR__ . '/resources/assets' => public_path('assets'),
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishResources();
+        }
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/ussd.php', 'ussd');
+    }
+
+    protected function publishResources()
+    {
+        $this->publishes([
+            __DIR__ . '/resources/lang' => resource_path('lang'),
+        ], 'ussd-lang');
+
+        $this->publishes([
+            __DIR__ . '/Repositories/Handler.php' => app_path('Repositories/Handler.php')
+        ], 'ussd-repositories');
+    }
+}
