00e5042d5ed73b0c0ac58779602e274aec65048e
Cyril Aguvasu <aguvasucyril@gmail.com>
2023-06-06 00:50:38 +0300

    added swahili and english transalations

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -4,6 +4,7 @@ use App\Models\User;
 if('saveUser'){
     function saveUser($userData){
         User::create([
+            'name' => 'Cyril Aguvasu',
             'msisdn' => $userData['msisdn'],
             'locale' => 'en'
         ]);
---
+++ src/resources/lang/sw/ussd.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'default_message' => "Karibu Stilinski Ussd Library",
+    'item_home' => 'First Menu',
+    'item_locale_saved' => 'Lugha Imebadilishwa',
+    'item_quit' => 'Quit',
+
+    'item_language_english' => "Kiingereza",
+    'item_language_swahili' => "Swahili",
+
+    'item_navigation_home' => 'Menu Kuu',
+    'item_navigation_next' => 'Next',
+    'item_navigation_previous' => 'Previous',
+    'item_navigation_back' => 'Back',
+    'item_message_quit' => 'Ahsante kwa kutembelea Stilinski Ussd Library. Endapo una swali piga 0705799644',
+    'item_contact_us' => 'Wasiliana Nasi',
+    'item_choose_language' => 'Badilisha Lugha',
+    'item_about_us' => 'About Us',
+
+    'enter_valid_input' => 'Invalid input',
+    'invalid_input' => 'Invalid input',
+
+    'welcome' => 'Karibu Stilinski Ussd Library :name',
+    'welcome_new_user' => "Karibu Stilinski Ussd Library :name",
+    'welcome_registered_user' => "Karibu Stilinski Ussd Library :name",
+
+    'item_display_users' => "Display Users",
+    'item_users' => "\n:first_name :surname :msisdn",
+
+];
